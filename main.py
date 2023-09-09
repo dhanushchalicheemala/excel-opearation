0471e5add30e830043b6a23c678c6db88c184b04
C Dhanush <103655255+dhanushchalicheemala@users.noreply.github.com>
2023-09-09 21:19:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ for i in range(2, tosheet.max_row+1):
     for j in range(2, fromsheet.max_row + 1):
         if tem.value == fromsheet.cell(j, 2).value:
             tosheet.cell(i, 14).value = fromsheet.cell(j, 24).value
+for row in range(2, from_excel.max_row + 1):
+    from_excel = from_excel.cell(row=row, column=9).value
+    from_excel= from_excel.cell(row=row, column=10).value
+   from_excel = from_excel.cell(row=row, column=11).value
 
+    if from_excel is None and from_excel is None and from_excel is None:
+        continue
+
+    total_3_months_cons = 3(from_excel)
+
+    from_excel.cell(row=row, column=13).value = total_3_months_cons
+    from_excel.cell(row=1, column=13).value = "Total Cons 3 Months"
 
 toexcel.save('new.xlsx')
